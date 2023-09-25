e381690345d942c7c9a2240cd2ad6e53d81a5cf1
regotiphaine <regotiphaine@10.3.61.155>
2023-09-25 11:12:24 +0200

    ajout de calculator, exo1 et 2

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author regotiphaine TDC Calculator 
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Please enter the operator\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+        System.out.print("Veuillez saisir une valeur entière : ");
+          int operateur = scanner.nextInt();
+    }
+    
+}
---
+++ Exo 1/src/exo/pkg1/Exo1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author regotiphaine TDC Découverte de Netbeans 25/09/2023
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in); System.out.println("Bonjour, quel est votre prenom ?"); prenom = sc.nextLine();
+    }
+    
+}
