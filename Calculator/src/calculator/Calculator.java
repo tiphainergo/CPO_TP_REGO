900c474f3cf88a0d8f1aeb1f9a8a06ae94ebd1cc
regotiphaine <regotiphaine@10.3.61.155>
2023-09-26 14:34:49 +0200

    modification exo calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -4,6 +4,8 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author regotiphaine TDC Calculator 
@@ -16,8 +18,53 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Please enter the operator\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
-        System.out.print("Veuillez saisir une valeur entière : ");
-          int operateur = scanner.nextInt();
+ 
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un nombre :");
+        int operateur = sc.nextInt();
+        
+        while (operateur>5 || operateur<0) {
+            System.out.println("ERREUR La valeur n'est pas comprise entre 0 et 5");
+            System.out.println("\n Entrer un nombre :");
+            operateur = sc.nextInt();
+        }
+        
+        
+                
+        System.out.println("\n Entrer une première valeur :");
+        float operande1 = sc.nextFloat();
+        
+        System.out.println("\n Entrer une deuxième valeur :");
+        float operande2 = sc.nextFloat();
+        
+        float result;
+        switch (operateur){
+            case 1 :// addition
+                result = operande1 + operande2;
+                System.out.println("Le résultat est :" + result);
+                break;
+            case 2 ://sustraction
+                result = operande1 - operande2;
+                System.out.println("Le résultat est :" + result);
+                break;
+            case 3://multiplication
+                result = operande1*operande2;
+                System.out.println("Le résultat est :" + result);
+                break;
+            case 4://division
+                result = operande1/operande2;
+                System.out.println("Le résultat est :" + result);
+                break;
+            case 5://modulo
+                result=operande1%operande2;
+                System.out.println("Le résultat est :" + result);
+                break;
+                        
+                        
+                    
+            
+        }
     }
     
 }
