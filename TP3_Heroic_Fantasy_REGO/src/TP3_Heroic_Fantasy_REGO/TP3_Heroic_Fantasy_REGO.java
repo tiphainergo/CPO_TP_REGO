db31846d109249ac7f6cc4ced7e7ac2c39b7ff9f
regotiphaine <regotiphaine@10.3.61.155>
2023-10-27 17:03:30 +0200

    TP3 : fin

--- TP3_Heroic_Fantasy_REGO/src/TP3_Heroic_Fantasy_REGO/TP3_Heroic_Fantasy_REGO.java
+++ TP3_Heroic_Fantasy_REGO/src/TP3_Heroic_Fantasy_REGO/TP3_Heroic_Fantasy_REGO.java
@@ -22,74 +22,80 @@ public class TP3_Heroic_Fantasy_REGO {
         /**
          * @param args the command line arguments
          */
-        public static void main(String[] args) {
-            Epee excalibur = new Epee("Excalibur", 7, 5); //création excalibur
-            Epee durandal = new Epee("Durandal", 4, 7); //création Durandal
-            Baton chene = new Baton("Chene", 4, 5); //création Chêne
-            Baton charme = new Baton("Charme", 5, 6); //création Charme
+    public static void main(String[] args) {
+        // Création des armes
+        Epee excalibur = new Epee("Excalibur", 7, 5);
+        Epee durandal = new Epee("Durandal", 4, 7);
+        Baton chene = new Baton("Chene", 4, 5);
+        Baton charme = new Baton("Charme", 5, 6);
 
-            ArrayList<Arme> armes = new ArrayList<>(); //tableau dynamique armes
-            armes.add(excalibur); //ajout excalibur au tableau
-            armes.add(durandal);
-            armes.add(chene);
-            armes.add(charme);
+        // Création d'une liste d'armes
+        ArrayList<Arme> armes = new ArrayList<>();
+        armes.add(excalibur);
+        armes.add(durandal);
+        armes.add(chene);
+        armes.add(charme);
 
-            for (Arme arme : armes) {
-                System.out.println(arme); //affichage des armes
-            }
-
-            //Lannister le guerrier
-            Guerrier lannister = new Guerrier("Lannister", 45, true);
-            //Conan le guerrier
-            Guerrier conan = new Guerrier("Conan", 78, false);
-            //Gandalf le magicien
-            Magicien gandalf = new Magicien("Gandalf", 65, true);
-            //Garcimore le guerrier
-            Magicien garcimore = new Magicien("Garcimore", 44, false);
-
-            ArrayList<Personnage> personnages = new ArrayList<>(); //tableau dynamique armes
-            personnages.add(gandalf); //ajout de gandalf au tableau
-            personnages.add(garcimore);
-            personnages.add(conan);
-            personnages.add(lannister);
-
-            for (Personnage personnage : personnages) {
-                System.out.println(personnage); //affichage personnages
-            }
-            //// GUERRIERS ////
-            conan.ajouterArme(excalibur); //on donne des armes à conan
-            conan.ajouterArme(durandal);
-            lannister.ajouterArme(excalibur);//on donne des armes à lannister
-            lannister.ajouterArme(durandal);
-            conan.equiperArme("Excalibur"); //conan prends excalibur
-            lannister.equiperArme("Durandal"); //lannister prends durandal
-
-            //// MAGICIENS ////
-            gandalf.ajouterArme(chene); //on donne des batons à gandalf
-            gandalf.ajouterArme(charme);
-            garcimore.ajouterArme(chene);//on donne des batons à garcimore
-            garcimore.ajouterArme(charme);
-            gandalf.equiperArme("Chene"); //gandalf prend chene
-            garcimore.equiperArme("Charme");//garcimore prend chene
-            System.out.println("Nb total personnage:" + Personnage.getNombrePersonnages());
-            System.out.println("Nb guerriers" + Guerrier.getNombreGuerriers());
-            System.out.println("Nb magiciens:" + Magicien.getNombreMagiciens());
-
-            //// COMBAT ENTRE GANDALF ET CONAN ////
-            System.out.println("\nAvant combat :");
-            System.out.println(conan); //affiche stats de conan avant le combat
-            System.out.println(gandalf);
-
-            conan.attaquer(gandalf);
-
-            System.out.println("\nApres combat :");
-            System.out.println(conan); //affiche stats de conan après le combat
-            System.out.println(gandalf);
-
-            System.out.println("\nLe guerrier est vivant : " + conan.estVivant()); //affiche si le personnage est encore en vie
-            System.out.println("Le magicien est vivant : " + gandalf.estVivant());
+        // Affichage des caractéristiques des armes
+        System.out.println("Liste des armes :");
+        for (Arme arme : armes) {
+            System.out.println(arme);
         }
-    
+
+        // Création des personnages
+        Guerrier lannister = new Guerrier("Lannister", 45, true);
+        Guerrier conan = new Guerrier("Conan", 78, false);
+        Magicien gandalf = new Magicien("Gandalf", 65, true);
+        Magicien garcimore = new Magicien("Garcimore", 44, false);
+
+        // Création d'une liste de personnages
+        ArrayList<Personnage> personnages = new ArrayList<>();
+        personnages.add(gandalf);
+        personnages.add(garcimore);
+        personnages.add(conan);
+        personnages.add(lannister);
+
+        // Affichage des caractéristiques des personnages
+        System.out.println("\nListe des personnages :");
+        for (Personnage personnage : personnages) {
+            System.out.println(personnage);
+        }
+
+        // Attribution et équipement des armes pour les guerriers
+        conan.ajouterArme(excalibur);
+        conan.ajouterArme(durandal);
+        lannister.ajouterArme(excalibur);
+        lannister.ajouterArme(durandal);
+        conan.equiperArme("Excalibur");
+        lannister.equiperArme("Durandal");
+
+        // Attribution et équipement des bâtons pour les magiciens
+        gandalf.ajouterArme(chene);
+        gandalf.ajouterArme(charme);
+        garcimore.ajouterArme(chene);
+        garcimore.ajouterArme(charme);
+        gandalf.equiperArme("Chene");
+        garcimore.equiperArme("Charme");
+
+        // Affichage du nombre total de personnages, de guerriers et de magiciens
+        System.out.println("\nNombre total de personnages : " + Personnage.getNombrePersonnages());
+        System.out.println("Nombre de guerriers : " + Guerrier.getNombreGuerriers());
+        System.out.println("Nombre de magiciens : " + Magicien.getNombreMagiciens());
+
+        // Combat entre Gandalf et Conan
+        System.out.println("\nAvant le combat :");
+        System.out.println(conan);
+        System.out.println(gandalf);
+
+        conan.attaquer(gandalf);
+
+        System.out.println("\nAprès le combat :");
+        System.out.println(conan);
+        System.out.println(gandalf);
+
+        // Vérification de la survie des personnages après le combat
+        System.out.println("\nLe guerrier est vivant : " + conan.estVivant());
+        System.out.println("Le magicien est vivant : " + gandalf.estVivant());
+    }
 }
-    
 
